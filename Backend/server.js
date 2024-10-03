b90f83f3f7488c8e754bc04c50afda6dce92baf0
Sean-D-231121 <231121@virtualwindow.co.za>
2024-10-03 12:06:19 +0200

    Added basic backend and frontend

---
+++ Backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static("public"));
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB is connected"))
+  .catch((err) => console.log(err));
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
